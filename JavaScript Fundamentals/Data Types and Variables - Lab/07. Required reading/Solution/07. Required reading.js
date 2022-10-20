2a5dfea7ac9f0bc55ccbbe21639149112e959b68
Krassi333 <krassi.pln@gmail.com>
2022-10-20 22:24:09 +0300

    Add new task

---
+++ JavaScript Fundamentals/Data Types and Variables - Lab/07. Required reading/Solution/07. Required reading.js
@@ -0,0 +1,12 @@
+function RequirRedeading(pages,pagesPerHour,days){
+let bookTime=pages/pagesPerHour;
+let hoursPerDay=bookTime/days;
+
+console.log(hoursPerDay);
+}
+
+RequirRedeading(432,
+
+    15 ,
+    
+    4)
