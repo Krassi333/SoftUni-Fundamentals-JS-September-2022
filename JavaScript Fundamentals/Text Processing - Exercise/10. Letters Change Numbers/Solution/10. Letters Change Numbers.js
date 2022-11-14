93c3c1de9d97cd8cae4a89e77f21041b52c58975
Krassi333 <krassi.pln@gmail.com>
2022-11-14 13:55:36 +0200

    Change the solution of task number 10

--- JavaScript Fundamentals/Text Processing - Exercise/10. Letters Change Numbers/Solution/10. Letters Change Numbers.js
+++ JavaScript Fundamentals/Text Processing - Exercise/10. Letters Change Numbers/Solution/10. Letters Change Numbers.js
@@ -1,5 +1,29 @@
-function lettersChangeNumbers(string){
+function lettersChangeNumbers(string) {
+    string = string.split(' ');
+    let sum = 0;
 
+    for (let line of string) {
+        if (line != '') {
+            let firstLetter = line[0];
+            let lastLetter = line[line.length - 1];
+            let number = Number(line.slice(1, line.length - 1));
+
+            if (firstLetter.charCodeAt(0) >= 97 && firstLetter.charCodeAt(0) <= 122) {
+                number *= firstLetter.charCodeAt(0) - 96;
+            } else {
+                number /= firstLetter.charCodeAt(0) - 64;
+            }
+
+            if (lastLetter.charCodeAt(0) >= 97 && lastLetter.charCodeAt(0) <= 122) {
+                number += lastLetter.charCodeAt(0) - 96;
+            } else {
+                number -= lastLetter.charCodeAt(0) - 64;
+            }
+            sum += number;
+        }
+
+    }
+    console.log(sum.toFixed(2));
 }
 
-lettersChangeNumbers('A12b s17G')
+lettersChangeNumbers('P34562Z q2576f   H456z')
